d1406187162978053083b465c2ef537ef78feb95
N.M.Dinura uthsara Nikalansuriya <87411492+Dinuraofficial@users.noreply.github.com>
2021-09-10 21:02:52 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -73,7 +73,7 @@ async def about(client, message):
 
 <b>♞ Library:</b> <a href="https://github.com/pyrogram/pyrogram">Pyrogram</a>
 
-<b>~ @DinuraNikalansuriya</b>""",
+ ~ @DinuraNikalansuriya</b>""",
      reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
